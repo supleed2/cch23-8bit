ee0e3042bbb6d3eba9873995250e05506d911c63
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-12-13 18:57:47 +0000

    Add http request logging

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ mod cal;
 
 #[shuttle_runtime::main]
 async fn main(#[shuttle_shared_db::Postgres] pool: sqlx::PgPool) -> shuttle_axum::ShuttleAxum {
-    Ok(axum::Router::new().nest("/", cal::router(pool)).into())
+    Ok(axum::Router::new()
+        .nest("/", cal::router(pool))
+        .layer(tower_http::trace::TraceLayer::new_for_http())
+        .into())
 }
